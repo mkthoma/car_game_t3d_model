f35410c87fc5b36c6d61ea7db1f7aaddb7a5e14b
mkthoma <mathewkennythomas@gmail.com>
2023-12-12 19:38:11 +0530

    code push

--- README.md
+++ README.md
@@ -1,1 +1,111 @@
-# car_game_t3d_model
+# Self-Driving Car Simulation with Reinforcement Learning
+
+This repository contains a self-driving car simulation implemented in Python using the Kivy framework. The self-driving car is equipped with a Deep Q Network (DQN) for learning and decision-making in its environment.
+
+The self-driving car simulation involves a car navigating through a 2D map with the goal of reaching specified targets. The car is equipped with sensors to detect the environment, and a DQN is used for decision-making. Users can interactively draw on the map to simulate regions with sand, affecting the car's behavior.
+
+- `ai.py`: Defines the Deep Q Network (DQN) and related components for reinforcement learning.
+- `map.py`: Main script for the Kivy-based self-driving car simulation.
+- `images`: Directory containing images used in the simulation.
+
+Before running, make sure you have the following also installed 
+```python
+conda install -c peterjc123 pytorch-cpu
+
+conda install kivy -c conda-forge
+```
+Run using the command 
+```python 
+python map.py
+```
+
+## Features
+
+- Interactive Map Drawing: Users can interactively draw on the map, simulating regions with sand that affect the self-driving car's behavior.
+- Target Navigation: The self-driving car navigates through a 2D map, aiming to reach specified targets.
+- Deep Q Network (DQN): The car's decision-making is facilitated by a DQN, a type of reinforcement learning algorithm.
+- Save and Load Functionality: Users can save the current state of the AI model and load the last saved state.
+
+## Deep Q Network (DQN)
+The self-driving car simulation incorporates a Deep Q Network (DQN) for decision-making. DQN is a type of reinforcement learning algorithm that enables the car to learn an optimal policy for navigating the environment. Here's a breakdown of the key aspects of the DQN implementation:
+
+### Neural Network Architecture (Network class in ai.py)
+
+- Input: The neural network takes a state vector as input, representing the current state of the environment. The state vector includes information such as sensor readings, orientation, and the presence of obstacles (sand) in the vicinity.
+
+- Architecture: 
+    - The neural network consists of three fully connected layers (fc1, fc2, fc3).
+    - The first layer (fc1) has 30 neurons and applies a rectified linear unit (ReLU) activation function.
+    - The second layer (fc2) has 100 neurons with a ReLU activation.
+    - The final layer (fc3) produces the Q-values for each possible action (3 actions in this case).
+- Output: The output of the neural network is a set of Q-values corresponding to each possible action. The action with the highest Q-value is selected as the agent's next move.
+
+### Experience Replay (ReplayMemory class in ai.py)
+- The DQN incorporates Experience Replay to enhance learning efficiency.
+- The ReplayMemory class manages a replay memory buffer that stores experiences (state, action, reward, next state) during the agent's interactions with the environment.
+- During training, random batches of experiences are sampled from the replay memory, breaking temporal correlations and improving the stability of the learning process.
+
+
+### Training (Dqn class in ai.py)
+
+- Learning Algorithm:
+    - The DQN employs the Q-learning algorithm to update the Q-values based on the Bellman equation, aiming to minimize the temporal difference error.
+    - The loss is calculated using the smooth L1 loss function.
+- Exploration-Exploitation:
+    - The agent selects actions using an epsilon-greedy strategy, balancing exploration (random actions) and exploitation (choosing the action with the highest Q-value).
+- Reward Scheme:
+    - Rewards and penalties are assigned based on the agent's interactions with the environment. Positive rewards are given for reaching targets, and negative rewards are assigned for collisions or deviations.
+
+## Targets
+The self-driving car simulation features three distinct targets that the car must navigate to. These targets are defined as follows:
+
+#### Target A1:
+Coordinates: (583, 710) aka Point 1. This is the initial target where the car begins its journey.
+
+#### Target A2:
+Coordinates: (628, 237) aka Point 2. After reaching Target A1, the car switches its target to A2.
+
+#### Target A3:
+Coordinates: (826, 262) aka Point 3. After reaching Target A2, the car switches its target to A3.
+
+The rotation of targets introduces a dynamic aspect to the navigation task, requiring the car to adapt its behavior based on the current target. The switching of targets allows for continuous learning and exploration of the environment.
+
+By navigating towards these targets and learning from its experiences, the self-driving car aims to optimize its policy for effective and efficient navigation in the simulated environment.
+
+## Simulation
+Video simulation of the program can be found [here](https://www.youtube.com/watch?v=gXsBGyxNsag) in youtube.
+
+## Observations
+
+### Reducing Temeperature
+When the temperature is reduced in the context of a softmax function, it affects the way probabilities are assigned to different actions. The softmax function is often used in reinforcement learning to convert raw action values (Q-values) into a probability distribution over actions. The temperature parameter, denoted as T, controls the "softness" or smoothness of this distribution.
+
+- Reduced Exploration: Lowering the temperature reduces the influence of lower Q-values, making it less likely for suboptimal actions to be chosen. This can limit the agent's exploration of alternative actions.
+- More Exploitation: With a lower temperature, the agent is more likely to exploit its current knowledge and consistently choose actions with higher Q-values.
+- Risk of Overfitting: Setting the temperature too low might lead to the agent becoming too deterministic and potentially overfitting to the current state of the environment, missing out on valuable exploration.
+
+Reducing the temperature in softmax makes the agent more focused on exploiting its current knowledge, potentially at the cost of reduced exploration.
+
+### Reducing Gamma (γ)
+
+The discount factor γ in reinforcement learning determines the importance of future rewards in the agent's decision-making process. Reducing γ has several effects on the agent's behavior and the learning process:
+
+- Short-Term Focus:A lower γ places more emphasis on immediate rewards and less on future rewards. The agent becomes more focused on obtaining quick rewards and is less concerned with the long-term consequences of its actions.
+
+- Impulsivity: A smaller γ makes the agent more impulsive, as it is less willing to wait for delayed rewards. It tends to favor actions that yield immediate positive outcomes.
+
+- Faster Learning in Short-Term: Lowering γ can lead to faster learning in the short term, as the agent adapts quickly to the local structure of the environment. However, this might come at the cost of potentially missing out on optimal long-term strategies.
+
+- Limited Exploration: With a low γ, the agent may become myopic and exploit its current knowledge without sufficiently exploring alternative actions. This can be a limitation in environments where delayed consequences are crucial for making optimal decisions.
+
+### Weak boundary signals
+
+- Agent's Decision-Making: If the "boundary-signal" is weak compared to the last reward, the agent may give more weight to the last reward in its decision-making process. The agent is likely to prioritize actions that led to the most recent positive or negative outcome, as it perceives this as more influential.
+
+- Temporal Considerations: The weakness of the "boundary-signal" relative to the last reward could result in the agent placing less importance on spatial or boundary-related information and more emphasis on the temporal aspects of the environment. The agent may become more myopic, focusing on short-term gains.
+
+- Risk of Ignoring Environmental Boundaries: If the "boundary-signal" is weak, the agent might be less sensitive to environmental boundaries or constraints. It may not give sufficient consideration to avoiding collisions or staying within specific regions of the environment.
+
+- Exploration Challenges: Weak boundary signals may impact the agent's ability to explore different regions of the environment, especially if these signals are essential for guiding the agent away from unsafe or unproductive areas.
+
+- Learning Bias: The agent may develop a learning bias towards actions that lead to immediate rewards, potentially overlooking actions that contribute to long-term success or adherence to environmental constraints.
---
+++ ai_td3.py
@@ -0,0 +1,325 @@
+import torch.optim as optim
+import numpy as np
+import gym
+from collections import deque
+import torch.nn.functional as F
+from torch.autograd import Variable
+import time
+from gym import wrappers
+import random
+import os
+import torch.autograd as autograd
+import torch
+import torch.nn as nn
+
+# AI for Self Driving Car
+
+# Importing the libraries
+
+
+# Creating the architecture of the Neural Network
+
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, 100)  # New FC layer
+        self.fc3 = nn.Linear(100, nb_action)  # New FC layer connecting to the output
+    
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))  # Apply activation to the output of the second FC layer
+        q_values = self.fc3(x)
+        return q_values
+
+# Implementing Experience Replay
+
+class ReplayMemory(object):
+    
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+    
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+    
+    def sample(self, batch_size):
+        samples = zip(*random.sample(self.memory, batch_size))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
+
+# Implementing Deep Q Learning
+
+class Dqn():
+    
+    def __init__(self, input_size, nb_action, gamma):
+        self.gamma = gamma
+        self.reward_window = []
+        self.model = Network(input_size, nb_action)
+        self.memory = ReplayMemory(100000)
+        self.optimizer = optim.Adam(self.model.parameters(), lr = 0.001)
+        self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        self.last_action = 0
+        self.last_reward = 0
+    
+    def select_action(self, state):
+        probs = F.softmax(self.model(Variable(state, volatile = True))*100) # T=100
+        action = probs.multinomial(1)
+        return action.data[0,0]
+    
+    def learn(self, batch_state, batch_next_state, batch_reward, batch_action):
+        outputs = self.model(batch_state).gather(1, batch_action.unsqueeze(1)).squeeze(1)
+        next_outputs = self.model(batch_next_state).detach().max(1)[0]
+        target = self.gamma*next_outputs + batch_reward
+        td_loss = F.smooth_l1_loss(outputs, target)
+        self.optimizer.zero_grad()
+        td_loss.backward(retain_graph = True)
+        self.optimizer.step()
+    
+    def update(self, reward, new_signal):
+        new_state = torch.Tensor(new_signal).float().unsqueeze(0)
+        self.memory.push((self.last_state, new_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward])))
+        action = self.select_action(new_state)
+        if len(self.memory.memory) > 100:
+            batch_state, batch_next_state, batch_action, batch_reward = self.memory.sample(100)
+            self.learn(batch_state, batch_next_state, batch_reward, batch_action)
+        self.last_action = action
+        self.last_state = new_state
+        self.last_reward = reward
+        self.reward_window.append(reward)
+        if len(self.reward_window) > 1000:
+            del self.reward_window[0]
+        return action
+    
+    def score(self):
+        return sum(self.reward_window)/(len(self.reward_window)+1.)
+    
+    def save(self):
+        torch.save({'state_dict': self.model.state_dict(),
+                    'optimizer' : self.optimizer.state_dict(),
+                   }, 'last_brain.pth')
+    
+    def load(self):
+        if os.path.isfile('last_brain.pth'):
+            print("=> loading checkpoint... ")
+            checkpoint = torch.load('last_brain.pth')
+            self.model.load_state_dict(checkpoint['state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            print("done !")
+        else:
+            print("no checkpoint found...")
+
+
+class ReplayBuffer(object):
+    
+  def __init__(self, max_size=1e6):
+    self.storage = []
+    self.max_size = max_size
+    self.ptr = 0
+
+  def add(self, transition):
+    if len(self.storage) == self.max_size:
+      self.storage[int(self.ptr)] = transition
+      self.ptr = (self.ptr + 1) % self.max_size
+    else:
+      self.storage.append(transition)
+      self.ptr = (self.ptr + 1) % self.max_size
+
+  def sample(self, batch_size):
+    ind = np.random.randint(0, len(self.storage), size=batch_size)
+    batch_states, batch_next_states, batch_actions, batch_rewards, batch_dones = [], [], [], [], []
+    for i in ind:
+      state, next_state, action, reward, done = self.storage[i]
+      batch_states.append(np.array(state, copy=False))
+      batch_next_states.append(np.array(next_state, copy=False))
+      batch_actions.append(np.array(action, copy=False))
+      batch_rewards.append(np.array(reward, copy=False))
+      batch_dones.append(np.array(done, copy=False))
+    return np.array(batch_states), np.array(batch_next_states), np.array(batch_actions), np.array(batch_rewards).reshape(-1, 1), np.array(batch_dones).reshape(-1, 1)
+
+class Actor(nn.Module):
+    
+  def __init__(self, state_dim, action_dim, max_action):
+    super(Actor, self).__init__()
+    self.layer_1 = nn.Linear(state_dim, 400)
+    self.layer_2 = nn.Linear(400, 300)
+    self.layer_3 = nn.Linear(300, action_dim)
+    self.max_action = max_action
+
+  def forward(self, x):
+    x = F.relu(self.layer_1(x))
+    x = F.relu(self.layer_2(x))
+    x = self.max_action * torch.tanh(self.layer_3(x))
+    return x
+
+class Critic(nn.Module):
+    
+  def __init__(self, state_dim, action_dim):
+    super(Critic, self).__init__()
+    # Defining the first Critic neural network
+    self.layer_1 = nn.Linear(state_dim + action_dim, 400)
+    self.layer_2 = nn.Linear(400, 300)
+    self.layer_3 = nn.Linear(300, 1)
+    # Defining the second Critic neural network
+    self.layer_4 = nn.Linear(state_dim + action_dim, 400)
+    self.layer_5 = nn.Linear(400, 300)
+    self.layer_6 = nn.Linear(300, 1)
+
+  def forward(self, x, u):
+    xu = torch.cat([x, u], 1)
+    # Forward-Propagation on the first Critic Neural Network
+    x1 = F.relu(self.layer_1(xu))
+    x1 = F.relu(self.layer_2(x1))
+    x1 = self.layer_3(x1)
+    # Forward-Propagation on the second Critic Neural Network
+    x2 = F.relu(self.layer_4(xu))
+    x2 = F.relu(self.layer_5(x2))
+    x2 = self.layer_6(x2)
+    return x1, x2
+
+  def Q1(self, x, u):
+    xu = torch.cat([x, u], 1)
+    x1 = F.relu(self.layer_1(xu))
+    x1 = F.relu(self.layer_2(x1))
+    x1 = self.layer_3(x1)
+    return x1
+
+# Selecting the device (CPU or GPU)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Building the whole Training Process into a class
+
+class TD3(object):
+
+  def __init__(self, state_dim, action_dim, max_action):
+    self.actor = Actor(state_dim, action_dim, max_action).to(device)
+    self.actor_target = Actor(state_dim, action_dim, max_action).to(device)
+    self.actor_target.load_state_dict(self.actor.state_dict())
+    self.actor_optimizer = torch.optim.Adam(self.actor.parameters())
+    self.critic = Critic(state_dim, action_dim).to(device)
+    self.critic_target = Critic(state_dim, action_dim).to(device)
+    self.critic_target.load_state_dict(self.critic.state_dict())
+    self.critic_optimizer = torch.optim.Adam(self.critic.parameters())
+    self.max_action = max_action
+
+  def select_action(self, state):
+    state = torch.Tensor(state.reshape(1, -1)).to(device)
+    return self.actor(state).cpu().data.numpy().flatten()
+
+  def train(self, replay_buffer, iterations, batch_size=100, discount=0.99, tau=0.005, policy_noise=0.2, noise_clip=0.5, policy_freq=2):
+
+    for it in range(iterations):
+
+      # Step 4: We sample a batch of transitions (s, s’, a, r) from the memory
+      batch_states, batch_next_states, batch_actions, batch_rewards, batch_dones = replay_buffer.sample(batch_size)
+      state = torch.Tensor(batch_states).to(device)
+      next_state = torch.Tensor(batch_next_states).to(device)
+      action = torch.Tensor(batch_actions).to(device)
+      reward = torch.Tensor(batch_rewards).to(device)
+      done = torch.Tensor(batch_dones).to(device)
+
+      # Step 5: From the next state s’, the Actor target plays the next action a’
+      next_action = self.actor_target(next_state)
+
+      # Step 6: We add Gaussian noise to this next action a’ and we clamp it in a range of values supported by the environment
+      noise = torch.Tensor(batch_actions).data.normal_(0, policy_noise).to(device)
+      noise = noise.clamp(-noise_clip, noise_clip)
+      next_action = (next_action + noise).clamp(-self.max_action, self.max_action)
+
+      # Step 7: The two Critic targets take each the couple (s’, a’) as input and return two Q-values Qt1(s’,a’) and Qt2(s’,a’) as outputs
+      target_Q1, target_Q2 = self.critic_target(next_state, next_action)
+
+      # Step 8: We keep the minimum of these two Q-values: min(Qt1, Qt2)
+      target_Q = torch.min(target_Q1, target_Q2)
+
+      # Step 9: We get the final target of the two Critic models, which is: Qt = r + γ * min(Qt1, Qt2), where γ is the discount factor
+      target_Q = reward + ((1 - done) * discount * target_Q).detach()
+
+      # Step 10: The two Critic models take each the couple (s, a) as input and return two Q-values Q1(s,a) and Q2(s,a) as outputs
+      current_Q1, current_Q2 = self.critic(state, action)
+
+      # Step 11: We compute the loss coming from the two Critic models: Critic Loss = MSE_Loss(Q1(s,a), Qt) + MSE_Loss(Q2(s,a), Qt)
+      critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(current_Q2, target_Q)
+
+      # Step 12: We backpropagate this Critic loss and update the parameters of the two Critic models with a SGD optimizer
+      self.critic_optimizer.zero_grad()
+      critic_loss.backward()
+      self.critic_optimizer.step()
+
+      # Step 13: Once every two iterations, we update our Actor model by performing gradient ascent on the output of the first Critic model
+      if it % policy_freq == 0:
+        actor_loss = -self.critic.Q1(state, self.actor(state)).mean()
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # Step 14: Still once every two iterations, we update the weights of the Actor target by polyak averaging
+        for param, target_param in zip(self.actor.parameters(), self.actor_target.parameters()):
+          target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+        # Step 15: Still once every two iterations, we update the weights of the Critic target by polyak averaging
+        for param, target_param in zip(self.critic.parameters(), self.critic_target.parameters()):
+          target_param.data.copy_(tau * param.data + (1 - tau) * target_param.data)
+
+  # Making a save method to save a trained model
+  def save(self, filename, directory):
+    torch.save(self.actor.state_dict(), '%s/%s_actor.pth' % (directory, filename))
+    torch.save(self.critic.state_dict(), '%s/%s_critic.pth' % (directory, filename))
+
+  # Making a load method to load a pre-trained model
+  def load(self, filename, directory):
+    self.actor.load_state_dict(torch.load('%s/%s_actor.pth' % (directory, filename)))
+    self.critic.load_state_dict(torch.load('%s/%s_critic.pth' % (directory, filename)))
+    
+    
+    
+env_name = "AntBulletEnv-v0" # Name of a environment (set it to any Continous environment you want)
+seed = 0 # Random seed number
+start_timesteps = 1e4 # Number of iterations/timesteps before which the model randomly chooses an action, and after which it starts to use the policy network
+eval_freq = 5e3 # How often the evaluation step is performed (after how many timesteps)
+max_timesteps = 5e5 # Total number of iterations/timesteps
+save_models = True # Boolean checker whether or not to save the pre-trained model
+expl_noise = 0.1 # Exploration noise - STD value of exploration Gaussian noise
+batch_size = 100 # Size of the batch
+discount = 0.99 # Discount factor gamma, used in the calculation of the total discounted reward
+tau = 0.005 # Target network update rate
+policy_noise = 0.2 # STD of Gaussian noise added to the actions for the exploration purposes
+noise_clip = 0.5 # Maximum value of the Gaussian noise added to the actions (policy)
+policy_freq = 2 # Number of iterations to wait before the policy network (Actor model) is updated
+
+env = gym.make(env_name)
+def evaluate_policy(policy, eval_episodes=10):
+    avg_reward = 0.
+    for _ in range(eval_episodes):
+        obs = env.reset()
+        done = False
+        while not done:
+            action = policy.select_action(np.array(obs))
+            obs, reward, done, _ = env.step(action)
+            avg_reward += reward
+    avg_reward /= eval_episodes
+    print ("---------------------------------------")
+    print ("Average Reward over the Evaluation Step: %f" % (avg_reward))
+    print ("---------------------------------------")
+    return avg_reward
+
+file_name = "%s_%s_%s" % ("TD3", env_name, str(seed))
+print ("---------------------------------------")
+print ("Settings: %s" % (file_name))
+print ("---------------------------------------")
+
+if not os.path.exists("./results"):
+      os.makedirs("./results")
+if save_models and not os.path.exists("./models"):
+  os.makedirs("./models")
+
+env.seed(seed)
+torch.manual_seed(seed)
+np.random.seed(seed)
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.shape[0]
+max_action = float(env.action_space.high[0])
